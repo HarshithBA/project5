7df82ebe6021aaed12da337ec954fbdc1f2977ec
HarshithBA <96473901+HarshithBA@users.noreply.github.com>
2022-01-04 16:34:13 +0530

    Add files via upload

--- squarecalculator.js
+++ squarecalculator.js
@@ -1,5 +1,8 @@
 function square() {
-  //1.Take the value of input box with id "n" and store it in a variable named "input". Wrap the input value in a Number() function
+  //1.Take the value of input box with id "n" and store it in a variable named "input". Wrap the input value in a Number() functi
+  var input=Number(document.getElementById("n").value);
   //2. To calculate square use this formula : input * input and assign it to a variable named "result"
+  var result=input*input;
   //3. Display the result in an HTML element. Set the innerHTML of HTML elemnt with id "answer" to result.
+  document.getElementById("answer").innerHTML=result;
 }
